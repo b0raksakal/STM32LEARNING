8f1e4585e1663a53019fc929d883f87ffe577bdf
Bora Aksakal <b0raksakal@gmail.com>
2025-06-25 22:09:24 +0300

    v3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -7,21 +7,29 @@
 
 #include "stm32f4xx_hal.h"
 
-void GPIO_Config(void);
+void GPIOA_Config(void);
+void GPIOC_Config(void);
 void delay(uint32_t delayVal);
 
 int main(void){
 
-	GPIO_Config();
+	GPIOA_Config();
+	GPIOC_Config();
+	int flag = 1;
 
 	while(1){
-
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-		delay(500000);
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 1 && flag == 1){
+			HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+			//delay(50000);
+			flag = 0;
+		}
+		else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 0){
+			flag = 1;
+		}
 	}
 }
 
-void GPIO_Config(){
+void GPIOA_Config(){
 	 __HAL_RCC_GPIOA_CLK_ENABLE();
 
 	 GPIO_InitTypeDef GPIOA_Init = {};
@@ -31,6 +39,18 @@ void GPIO_Config(){
 
 	 HAL_GPIO_Init(GPIOA, &GPIOA_Init);
 }
+
+void GPIOC_Config(){
+	 __HAL_RCC_GPIOC_CLK_ENABLE();
+
+	 GPIO_InitTypeDef GPIOC_Init = {};
+
+	 GPIOC_Init.Pin = GPIO_PIN_13;
+	 GPIOC_Init.Mode = GPIO_MODE_INPUT;
+	 GPIOC_Init.Pull = GPIO_NOPULL;
+	 HAL_GPIO_Init(GPIOC, &GPIOC_Init);
+}
+
 
 void delay(uint32_t delayVal){
 	uint32_t i;
